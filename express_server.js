84625520155b0390d19f54f60f39178c2b5a9d51
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-11 02:10:25 +0000

    played with POST

--- express_server.js
+++ express_server.js
@@ -9,12 +9,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 }
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 app.get("/", (req, res) => {
   res.send("Hello, mortal.");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/hello", (req, res) => {
@@ -39,6 +52,10 @@ app.get("/set", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
 });
+
+function generateRandomString() {
+  return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port: ${PORT}!`);
